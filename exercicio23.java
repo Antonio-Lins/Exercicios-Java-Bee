7f98e3ddd7bb610c4ae7b0db8cf33acc590d6f17
Antonio Lins <lins240608@gmail.com>
2025-03-05 15:16:39 -0300

    exercicio do beecrowd | 1035

--- exercicio23.java
+++ exercicio23.java
@@ -12,7 +12,7 @@ public class exercicio23 {
         if (b > c && d > a && (c + d) > (a + b) && c > 0 && d > 0 && a % 2 == 0) {
             System.out.println("Valores aceitos");
         }
-        else{
+        else{ 
             System.out.println("Valores nao aceitos");
         }
 
