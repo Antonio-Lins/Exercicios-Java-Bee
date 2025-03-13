6e302001d825d4e64be5ecdbaf4290aa45d135c8
Antonio Lins <lins240608@gmail.com>
2025-03-12 22:19:01 -0300

    exercicio do beecrowd | 1046

---
+++ exercicio33.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class exercicio33 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int hora = 24;
+
+        if (B > A) {
+            int Duração = B - A;
+            System.out.printf("O JOGO DUROU %d HORA(S)" , Duração);
+        }
+
+        
+        else if (B < A) {
+            int Duração = (hora - A) + B;
+            System.out.printf("O JOGO DUROU %d HORA(S)" , Duração);
+        }
+
+        
+        else if (B == A) {
+            System.out.printf("O JOGO DUROU 24 HORA(S)");
+        }
+
+
+        scanner.close();
+    }
+}
